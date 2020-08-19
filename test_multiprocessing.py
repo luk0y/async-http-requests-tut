90dc8747b8545b69d2decdde7d2494ce1bae658a
luk0y <45989946+luk0y@users.noreply.github.com>
2020-08-19 11:41:46 +0530

    Update test_multiprocessing.py

--- test_multiprocessing.py
+++ test_multiprocessing.py
@@ -11,9 +11,9 @@ def fetch(session, url):
     with session.get(url) as response:
         print(response.json()['uuid'])
 
-
-@timer(1, 1)
-def main():
-    with Pool() as pool:
-        with requests.Session() as session:
-            pool.starmap(fetch, [(session, URL) for _ in range(100)])
+if __name__ == "__main__":
+    @timer(1, 1)
+    def main():
+        with Pool() as pool:
+            with requests.Session() as session:
+                pool.starmap(fetch, [(session, URL) for _ in range(100)])
